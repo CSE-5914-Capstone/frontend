2ee8c03389f78253afa7ff3a3c866d1180d0c99f
dylansussman <dylansussman17@gmail.com>
2024-01-30 13:47:21 -0500

    Plain page with search bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import Container from "@material-ui/core/Container"
+import { IconButton, InputBase, Paper } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <Container>
+      <Paper component="form">
+        <InputBase 
+          placeholder='Search'
+        />
+        <IconButton type="submit" aria-label='search'>
+          <SearchIcon />
+        </IconButton>
+      </Paper>
+    </Container>
   );
 }
 

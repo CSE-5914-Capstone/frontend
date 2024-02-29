1992ba2d09c79494832138bf1e8e78e88bb31981
Jolie Sebel <jolisebel@gmail.com>
2024-02-29 16:26:41 -0500

    Added created playlist functionality, need to connect to spotify API (and add search functionality stil)

---
+++ src/components/Playlist.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import SongsList from './SongsList';
+import React, {useState, useEffect} from 'react';
+
+
+function Playlist({songName}){
+    const [songs, setSongs] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+    }, []); 
+    
+    const fetchData = () => {
+        //songName will be passed in as a parameter here
+        axios.get("http://localhost:3000/playlist-from-song")
+        .then((response) => {
+            let data = response.data;
+            let searchResults = [];
+
+            if (Array.isArray(data)) {
+                data.forEach((song) => {
+                searchResults.push({
+                    name: song["track_name"],
+                    artist: song["track_artist"],
+                    genre: song["playlist_genre"],
+                    bpm: Math.round(song["tempo"]),
+                });
+                });
+            }
+
+            setSongs(searchResults);
+        })
+        .catch((error) => {
+            console.error("Error fetching data:", error);
+        });
+    };
+
+    return(<SongsList songs={songs} />)
+}
+
+export default Playlist;

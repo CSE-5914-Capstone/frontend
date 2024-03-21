d4d81db3ce945ac39fd215151d442641ec733779
Jolie Sebel <jolisebel@gmail.com>
2024-03-21 12:10:40 -0400

    Fixed Issue

--- src/App.js
+++ src/App.js
@@ -11,60 +11,17 @@ function TabPanel({ value, index, children }) {
         children
       )}
     </Container>
-  );
+  )
 }
 
 function App() {
   const classes = useStyles();
-  const [songs, setSongs] = useState([]);
-  const [originalColors, setOriginalColors] = useState([]);
-  const [currentTab, setCurrentTab] = useState(0);
-  const [searchKey, setSearchKey] = useState('');
-  const [numberOfSongs, setNumberOfSongs] = useState(1);
-  const [maxSongs, setMaxSongs] = useState(0);
-
-  useEffect(() => {
-    fetchData();
-  }, [searchKey, numberOfSongs]);
-
-  const fetchData = () => {
-    axios.get(`http://localhost:3000/playlist-from-song?searchKey=${searchKey}`)
-      .then((response) => {
-        let data = response.data;
-        let searchResults = [];
-
-        if (Array.isArray(data)) {
-          setMaxSongs(data.length);
-          data.slice(0, numberOfSongs).forEach((song, index) => {
-            searchResults.push({
-              name: song["track_name"],
-              artist: song["track_artist"],
-              genre: song["playlist_genre"],
-              bpm: Math.round(song["tempo"]),
-            });
-          });
-        }
-
-        setSongs(searchResults);
-        setOriginalColors(Array(searchResults.length).fill().map((_, index) => getNextColor(index)));
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
-  };
+  const [currentTab, setCurrentTab] = useState(0)
 
   const onTabChange = (event, newTab) => {
-    setCurrentTab(newTab);
-  };
-
-  const clearSearch = () => {
-    setSearchKey('');
-  };
-
-  const handleSliderChange = (event, newValue) => {
-    setNumberOfSongs(newValue);
-  };
-
+    setCurrentTab(newTab)
+  }
+  
   return (
     <Container className={classes.appContainer}>
       <AppBar className={classes.navBarContainer} position='static' color='default' >
@@ -78,21 +35,7 @@ function App() {
           <Logo /> {/* Render your logo component here */}
         </Container>
         <TabPanel value={currentTab} index={0}>
-          <div className={classes.sliderContainer}>
-            <Typography id="song-slider" gutterBottom>
-              Number of Songs to Display: {numberOfSongs}
-            </Typography>
-            <Slider
-              value={numberOfSongs}
-              onChange={handleSliderChange}
-              aria-labelledby="song-slider"
-              step={1}
-              marks
-              min={1}
-              max={maxSongs}
-            />
-          </div>
-          <SearchSongs setSongs={setSongs} songs={songs} colors={originalColors} clearSearch={clearSearch} />
+          <HomePage></HomePage>
         </TabPanel>
         <TabPanel value={currentTab} index={1}>
           <CreatePlaylist></CreatePlaylist>
@@ -101,6 +44,8 @@ function App() {
     </Container>
   );
 }
+
+export default App;
 
 const useStyles = makeStyles((theme) => ({
   appContainer: {
@@ -118,9 +63,16 @@ const useStyles = makeStyles((theme) => ({
     textAlign: 'center',
     marginBottom: theme.spacing(3),
   },
-  sliderContainer: {
-    width: '50%',
-    margin: 'auto',
-    marginTop: theme.spacing(2),
+  searchBar: {
+    border: '2px solid black',
+    borderRadius: theme.spacing(1),
   },
+  searchBarContainer: {
+    marginBottom: theme.spacing(3),
+    textAlign: 'center', // Center the search bar
+  },
+  songCount: {
+    textAlign: 'center', // Center the text
+    marginBottom: theme.spacing(2),
+  }
 }));
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,16 +1,5 @@
-// src/components/HomePage.js
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-const HomePage = () => {
-  return (
-    <div>
-      <h1>Welcome to My Playlist App</h1>
-      <Link to="/app">
-        <button>Create Playlist</button>
-      </Link>
-    </div>
-  );
+function HomePage(){
+    return(<p>Home Page</p>)
 }
 
-export default HomePage;
+export default HomePage
